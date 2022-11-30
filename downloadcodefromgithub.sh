ca38fc7f568b5b77f0e4e8c2f7bc070bc7cf594e
dharlabwustl <atulkumar@wustl.edu>
2022-11-30 15:01:59 -0600

    Commit 1

--- downloadcodefromgithub.sh
+++ downloadcodefromgithub.sh
@@ -5,12 +5,6 @@ git clone ${gitlink}
 program_dir_name=$(basename ${gitlink} )
 program_dir_name=${program_dir_name%.git}
 mv ${program_dir_name}/* /software/
-chmod +x /software/*.sh 
-#INPUTFILENAME=${1}
-#OUTPUTDIRNAME=/workingoutput #${2}
-#SESSION_ID=${1}
-#XNAT_USER=${2}
-#XNAT_PASS=${3}
-#TYPE_OF_PROGRAM=${5}
+chmod +x /software/*.sh
 
 /software/script_to_call_main_program.sh  #${INPUTFILENAME} ${OUTPUTDIRNAME} #$SESSION_ID $XNAT_USER $XNAT_PASS ${TYPE_OF_PROGRAM}
