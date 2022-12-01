65b86437418fb7d34cddbdaace0e667e9c5bceae
dharlabwustl <atulkumar@wustl.edu>
2022-12-01 14:53:41 -0600

    Commit 4

--- rundockercommand.sh
+++ rundockercommand.sh
@@ -10,9 +10,9 @@ mkdir ZIPFILEDIR
 mkdir outputinsidedocker
 mkdir software 
 sessionID=SNIPR_E03614 #SNIPR_E03516
-#rm -r workingoutput/*
-#rm -r ZIPFILEDIR/*
-#rm -r outputinsidedocker/*
+rm -r workingoutput/*
+rm -r ZIPFILEDIR/*
+rm -r outputinsidedocker/*
 rm -r software/*
 docker run --gpus all -v $PWD/software:/software -v $PWD/workinginput:/workinginput -v $PWD/workingoutput:/workingoutput -v $PWD/ZIPFILEDIR:/ZIPFILEDIR -v $PWD/outputinsidedocker:/outputinsidedocker  -it sharmaatul11/${imagename}   /callfromgithub/downloadcodefromgithub.sh https://github.com/dharlabwustl/ATELECTASIS.git ##### ${sessionID} $XNAT_USER $XNAT_PASS https://snipr-dev-test1.nrg.wustl.edu 2 ##/software/call_combine_all_edemabiomarker_csvfile_projectlevel.sh  #${sessionID} $XNAT_USER $XNAT_PASS $XNAT_HOST #https://snipr-dev-test1.nrg.wustl.edu
 rm -r software/*
