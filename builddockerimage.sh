1698a6a71b8a07c56373b1283bc88c7285e3d13d
dharlabwustl <atulkumar@wustl.edu>
2023-01-19 12:01:10 -0600

    Commit 4

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -1,13 +1,17 @@
-./bashtowriteDockerfile.sh
+parent_dir=${2}
+${parent_dir}/bashtowriteDockerfile.sh
 echo "  "
 command=""
-for x in *.json ;
+for x in ${parent_dir}/*.json ;
 do 
 	command="${command}   ${x}  "
 done
 echo $command
 python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ./Dockerfile
- imagename=$1
+imagename=$1
 
-docker build -t sharmaatul11/${imagename} . 
+docker build -t sharmaatul11/${imagename} ${parent_dir}
 docker push sharmaatul11/${imagename}
+
+docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} ${parent_dir}
+docker push registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename}
--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -1,25 +1,43 @@
 #!/bin/bash 
 
-#SESSION_ID=${1}
-#XNAT_USER=${2}
-#XNAT_PASS=${3}
-#TYPE_OF_PROGRAM=${4}
-#INPUTFILENAME=${1}
-OUTPUTDIRNAME=/workingoutput #${2}
-#############################LUNG SEGMENTATION ##########################################
-for INPUTFILENAME in /workinginput/*.nii* ;
-do
-/opt/conda/envs/rapids/bin/python /software/lungsegmentation_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME}
-done
-################################### VESSEL SEGMENTATION ###############################################
-for INPUTFILENAME in /workinginput/*.nii* ;
-do
-/opt/conda/bin/python /software/vessel_segmentation_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME}
-done
-CALCULATION_DIR=/calculation
-LATEX_DIR=/latex
-IMAGES_DIR=/images
-for INPUTFILENAME in /workinginput/*.nii* ;
-do
-/opt/conda/bin/python  /software/atelectatis_perc_cal_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME} ${IMAGES_DIR} ${CALCULATION_DIR} ${LATEX_DIR}
-done
+SESSION_ID=${1}
+XNAT_USER=${2}
+XNAT_PASS=${3}
+TYPE_OF_PROGRAM=${4}
+export XNAT_HOST=${5}
+echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
+/software/lungatelectasis_calculationJan19_2023.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+
+#if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
+#then
+#    /software/nwucalculation_session_level_allsteps_November14_2022.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+#fi
+#if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
+#then
+#    /software/dicom2nifti_call_sessionlevel_selected.sh  ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi
+#
+#if [[ ${TYPE_OF_PROGRAM} == 3 ]] ;
+#then
+#  PROJECT_ID=${1}
+#    /software/combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh  ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi
+#
+#if [[ ${TYPE_OF_PROGRAM} == 4 ]] ;
+#then
+#
+#   /software/nwucalculation_scan_level_allsteps.sh
+#fi
+#
+#
+#if [[ ${TYPE_OF_PROGRAM} == 5 ]] ;
+#then
+#
+#   /software/nwucalculation_onlocalcomp_aftersegJan172022.sh
+#fi
+#if [[ ${TYPE_OF_PROGRAM} == 6 ]] ;
+#then
+#
+#   /software/combine_csvs_and_copy_pdfs_projectlevel_Jan17_2023_LocalComputer.sh
+#fi
+
