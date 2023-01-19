5c3199a9ee8b56285621a9cfdd04211bd3b8d5d0
dharlabwustl <atulkumar@wustl.edu>
2023-01-19 12:01:31 -0600

    Commit 4

---
+++ script_to_call_main_program1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+#SESSION_ID=${1}
+#XNAT_USER=${2}
+#XNAT_PASS=${3}
+#TYPE_OF_PROGRAM=${4}
+#INPUTFILENAME=${1}
+OUTPUTDIRNAME=/workingoutput #${2}
+#############################LUNG SEGMENTATION ##########################################
+for INPUTFILENAME in /workinginput/*.nii* ;
+do
+/opt/conda/envs/rapids/bin/python /software/lungsegmentation_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME}
+done
+################################### VESSEL SEGMENTATION ###############################################
+for INPUTFILENAME in /workinginput/*.nii* ;
+do
+/opt/conda/bin/python /software/vessel_segmentation_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME}
+done
+CALCULATION_DIR=/calculation
+LATEX_DIR=/latex
+IMAGES_DIR=/images
+for INPUTFILENAME in /workinginput/*.nii* ;
+do
+/opt/conda/bin/python  /software/atelectatis_perc_cal_module.py ${INPUTFILENAME} ${OUTPUTDIRNAME} ${IMAGES_DIR} ${CALCULATION_DIR} ${LATEX_DIR}
+done
