e40f18624a0cefe2c64ae1dca8938b4d27d8c5a7
dharlabwustl <atulkumar@wustl.edu>
2023-07-03 14:16:20 -0500

    Commit 4

--- create_flowchart.sh
+++ create_flowchart.sh
@@ -371,7 +371,7 @@ copy_scan_data ${niftifile_csvfilename} ${working_dir}
 #done
 
 final_output_directory=/outputinsidedocker
-#while IFS=',' read -ra array; do
+while IFS=',' read -ra array; do
 scanID=1 #${array[2]}
 echo sessionId::${sessionID}
 #echo scanId::${scanID}
@@ -394,7 +394,7 @@ echo '${gray_image_filename} ${lung_mask} ${curvature_mask} ${atelectasis_mask} 
 call_create_imagesfor_presentation_arguments=('call_create_imagesfor_presentation' ${gray_image_filename} ${lung_mask} ${curvature_mask} ${atelectasis_mask} ${savetodir})
 outputfiles_present=$(python utilities_simple_forlungproject.py "${call_create_imagesfor_presentation_arguments[@]}")
 echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
-#done < <( tail -n +2 "${niftifile_csvfilename}" )
+done < <( tail -n +2 "${niftifile_csvfilename}" )
 #slice_number=
 #CALCULATION_DIR=/calculation
 #LATEX_DIR=/latex
