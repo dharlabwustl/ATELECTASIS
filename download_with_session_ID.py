54462ffc29592637abea66b5d5277e26c9a0b1bf
dharlabwustl <atulkumar@wustl.edu>
2023-07-03 14:02:27 -0500

    Commit 4

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -111,7 +111,7 @@ def download_files_in_scans_resources_withname_sh():
 
                         # print("row::{}".format(row))
                         # download_a_singlefile_with_URLROW(row,dir_to_save)
-                        download_a_singlefile_with_URIString(row_scan_resources['URI'],row['Name'],dir_to_save)
+                        download_a_singlefile_with_URIString(row_scan_resources['URI'],row_scan_resources['Name'],dir_to_save)
                         print("DOWNLOADED ::{}".format(row))
                         print("PASSED AT ::{}".format("download_files_in_a_resource"))
 
